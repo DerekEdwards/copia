153718dcaf68de8e2071bfbae50e199c463befa0
Derek Edwards <dedwards8@gmail.com>
2014-08-03 14:05:32 -0400

    Add Requests to user profile page.

--- app/models/comrade.rb
+++ app/models/comrade.rb
@@ -2,4 +2,12 @@ class Comrade < ActiveRecord::Base
 
   has_many :items 
 
+  def from_transactions
+    Transaction.where(requestee: self)
+  end
+
+  def to_transactions
+    Transaction.where(requestor: self)
+  end 
+
 end
